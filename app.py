2210dfef30ae920cc9ade3ca58a795c3fd0fa83b
Sreelakshmi BS <sreelekshmibs90@gmail.com>
2026-06-08 21:39:16 +0530

    first commit

--- app.py
+++ app.py
@@ -37,12 +37,22 @@ app.secret_key = os.getenv("SECRET_KEY")
 # Database Configuration
 # ============================================================
 
-app.config['MYSQL_HOST']     = 'localhost'       # MySQL is running on the same machine
-app.config['MYSQL_USER']     = 'root'            # Database username
-app.config['MYSQL_PASSWORD'] = 'Sree@2611'       # Database password
-app.config['MYSQL_DB']       = 'expense_tracker' # The database we're working with
-app.config['MYSQL_CHARSET']  = 'utf8mb4'         # Full Unicode support (handles emojis too)
+# for local development
+# app.config['MYSQL_HOST']     = 'localhost'       # MySQL is running on the same machine
+# app.config['MYSQL_USER']     = 'root'            # Database username
+# app.config['MYSQL_PASSWORD'] = 'Sree@2611'       # Database password
+# app.config['MYSQL_DB']       = 'expense_tracker' # The database we're working with
+# app.config['MYSQL_CHARSET']  = 'utf8mb4'         # Full Unicode support (handles emojis too)
 
+#for production
+import os
+
+app.config['MYSQL_HOST']     = os.getenv('MYSQL_HOST')
+app.config['MYSQL_PORT']     = int(os.getenv('MYSQL_PORT', 3306))
+app.config['MYSQL_USER']     = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB']       = os.getenv('MYSQL_DB')
+app.config['MYSQL_CHARSET']  = 'utf8mb4'
 # Bind the MySQL extension to our Flask app
 mysql = MySQL(app)
 
